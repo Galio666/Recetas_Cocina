8e4c028db5416ab6b94267b0e1e53efefaf28966
Galio666 <ascencio847@gmail.com>
2020-01-13 14:49:59 -0600

    Agrego desplegar datos en Recyclerview corrigio erro de desplegar imagen

--- app/src/main/java/mx/edu/uacm/recetas_cocina/AdaptadorRecetaFirebase.java
+++ app/src/main/java/mx/edu/uacm/recetas_cocina/AdaptadorRecetaFirebase.java
@@ -1,6 +1,9 @@
 package mx.edu.uacm.recetas_cocina;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +15,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 
-import com.squareup.picasso.Picasso;
-
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
 
@@ -27,6 +29,18 @@ public class AdaptadorRecetaFirebase extends RecyclerView.Adapter <AdaptadorRece
         this.detallesrecetas = detallesrecetas;
     }
 
+    public Bitmap dislayImagen(String url){
+        Bitmap bmp = null;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        //bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        //url =  Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        imageBytes=Base64.decode(url,Base64.DEFAULT);
+        Bitmap deBitmap= BitmapFactory.decodeByteArray(imageBytes,0,imageBytes.length);
+
+        return deBitmap;
+    }
+
     @NonNull
     @Override
     public AdaptadorRecetaFirebase.ViewHolderRecetas onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -38,7 +52,7 @@ public class AdaptadorRecetaFirebase extends RecyclerView.Adapter <AdaptadorRece
 
         holder.textViewNombreReceta.setText(detallesrecetas.get(position).getNombre());
         holder.textViewTipoReceta.setText(detallesrecetas.get(position).getCategoria());
-        Picasso.get().load(detallesrecetas.get(position).getFoto()).into(holder.imagen);
+        holder.imagen.setImageBitmap(dislayImagen(detallesrecetas.get(position).getFoto()));
 
         holder.dificultad.setNumStars(detallesrecetas.get(position).getCalificacion());
 
@@ -64,6 +78,9 @@ public class AdaptadorRecetaFirebase extends RecyclerView.Adapter <AdaptadorRece
            dificultad = (RatingBar)itemView.findViewById(R.id.Dificultad);
 
         }
+
+
+
 
 
         /*
